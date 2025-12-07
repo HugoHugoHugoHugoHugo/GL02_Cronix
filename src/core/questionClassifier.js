afe4e6ad2e7568f0faa183ecb46a551aab083360
Jcsssss <charletj@utt.fr>
2025-12-07 21:09:20 +0100

    Update questionClassifier.js
    Adaptation questionClassifier suite à mes modifs des autres fonctions

--- src/core/questionClassifier.js
+++ src/core/questionClassifier.js
@@ -2,6 +2,9 @@
 // Détection du type de question GIFT
 
 export function classifyQuestion(q) {
+  // Si type déjà défini par le parser
+  if (q.type && q.type !== "inconnu") return q.type;
+
   const answers = q.answers;
 
   // Si aucune réponse → question ouverte
@@ -9,7 +12,10 @@ export function classifyQuestion(q) {
 
   // Vrai/Faux
   if (answers.length === 1 &&
-      (answers[0].toLowerCase() === "true" || answers[0].toLowerCase() === "false")) {
+      (answers[0].toLowerCase() === "true" || 
+       answers[0].toLowerCase() === "false" ||
+       answers[0] === "T" || 
+       answers[0] === "F")) {
     return "vrai_faux";
   }
 
